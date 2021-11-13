f3b22a0ffef37e5c5dbab12a385268ec22a576b3
Md. Jalal Uddin Chowdhury <JalalChowdhuryCSE1999@gmail.com>
2021-11-13 18:07:36 +0600

    done

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const ObjectId = require('mongodb').ObjectId;
 
 const app = express()
 
-const port = process.env.PORT || 5000
+const port = process.env.PORT || 5000 ;
 
 app.use(cors());
 app.use(express.json());
